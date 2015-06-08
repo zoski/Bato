ef575d1ffde5e932b663e756494c5019bf67d0fd
ScrutchWasTaken <estelle.moncollin@gmail.com>
2015-06-08 11:29:00 +0200

    camera following the boat

--- js/init.js
+++ js/init.js
@@ -6,6 +6,12 @@ camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight
 camera.position.z = 5;
 camera.position.y = 2;
 controls = new THREE.OrbitControls( camera );
-renderer = new THREE.WebGLRenderer(  {antialias: true} );
+//set the min/max distance between the camera and the boat
+controls.minDistance = 3;
+controls.maxDistance = 10;
+//set the min/max angle to unable being under the sea
+controls.minPolarAngle = THREE.Math.degToRad(0);
+controls.maxPolarAngle = THREE.Math.degToRad(90);
+renderer = new THREE.WebGLRenderer(  {antialias: false} );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
